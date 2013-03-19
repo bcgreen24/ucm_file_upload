5e2d9edeb7ab93732cf52ded7e8273d1cbe1cfbf
Bryan Green <bcgreen@gmail.com>
2013-03-19 11:17:14 -0700

    Adding ability to upload multiple files at once

---
+++ file_upload.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+   $('.form-add_file_uploader').click(function(){
+       alert('hello');
+   })
+});
